57da9a69288f9bcac44c23ede7925fae4201cb13
Axrorbek <axrorbekaminjonov123@gmail.com>
2026-02-05 13:41:21 +0500

    new changes

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -74,8 +74,8 @@ class _DetailsScreenState extends State<DetailsScreen> {
               ),
               Positioned(
                 bottom: -90,
-
                 child: Container(
+                  padding: const EdgeInsets.all(16),
                   width: 327,
                   height: 180,
                   decoration: BoxDecoration(
@@ -86,6 +86,65 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         color: Colors.black.withAlpha(38),
                         blurRadius: 14,
                         offset: Offset(2, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Sealink Luxury Resort",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            width: 44,
+                            height: 44,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Color(0xff163C9F),
+                            ),
+                            child: SvgPicture.asset("assets/icons/Call.svg"),
+                          ),
+                        ],
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Florence",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xffBDBDBD),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 23),
+                      Row(
+                        children: [
+                          Text(
+                            "\$81",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff163C9F),
+                            ),
+                          ),
+                          const SizedBox(width: 6),
+                          Text(
+                            "/ A night",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xffBDBDBD),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
